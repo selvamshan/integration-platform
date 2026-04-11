f36ea38f965a29df02924d7a35d8783bf33ad93b
Selvaraj <selvaraj_sr4@hotmail.com>
2026-04-11 19:03:45 +0530

    set up in ubuntu

--- build-test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Testing local build..."
-cd crates/common && cargo check 2>&1 | head -20
-cd ../integration-runtime && cargo check 2>&1 | head -20
-cd ../data-plane && cargo check 2>&1 | head -20
-cd ../control-plane && cargo check 2>&1 | head -20
